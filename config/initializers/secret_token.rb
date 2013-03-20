b54960ed34fff4f7c617384f2ef2fb0083bb5c94
eg4000 <eg4000@gmail.com>
2013-03-20 22:07:20 +0200

    1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Temp::Application.config.secret_token = '61ebdfffa6a3b3ae4ab018e306318687faf403dde9b54ed55182fc7b5409454d6740706e1c96f30d74a0ba8d8b69da1a9ab655c6b25fde34c19ff78040197e4a'
